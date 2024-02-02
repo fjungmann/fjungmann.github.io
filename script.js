534b124c23cee43c3a5349f5c3cf1f0e45473daa
fjungmann <fabian.jungmann@gmail.com>
2024-02-02 10:46:03 +0000

    First attempt at a basic website

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLink(url) {
+    window.open(url, '_blank');
+}
+
+function navigateToWeatherGenerator() {
+    // Navigate to Weather Generator Page
+    window.location.href = 'weather.html'; // Adjust as per your weather generator page
+}
+
+function showAlert() {
+    alert('Coming Soon!');
+}
