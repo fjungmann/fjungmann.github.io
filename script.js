02871072e39c61584c45cd80818bc418273e7847
fjungmann <fabian.jungmann@gmail.com>
2024-02-02 12:31:00 +0000

    Matched style to app
    Added buttons, race data and style changes

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+
+function fetchAndUpdateRaceInfo() {
+    var databaseRef = firebase.database().ref();
+
+    // Fetch and update race date
+    databaseRef.child("race_date").on('value', function(snapshot) {
+        document.getElementById("raceDate").innerText = (snapshot.val() || "No data");
+    });
+
+    // Fetch and update race location
+    databaseRef.child("race_location").on('value', function(snapshot) {
+        document.getElementById("raceLocation").innerText = (snapshot.val() || "No data");
+    });
+
+    // Fetch and update race time
+    databaseRef.child("race_time").on('value', function(snapshot) {
+        document.getElementById("raceTime").innerText = (snapshot.val() || "No data");
+    });
+
+    // Fetch and update weather1
+    databaseRef.child("weather1").on('value', function(snapshot) {
+        document.getElementById("weather1").innerText = (snapshot.val() || "No data");
+    });
+
+    // Fetch and update weather2
+    databaseRef.child("weather2").on('value', function(snapshot) {
+        document.getElementById("weather2").innerText = (snapshot.val() || "No data");
+    });
+}
+fetchAndUpdateRaceInfo();
+
 function openLink(url) {
     window.open(url, '_blank');
 }
